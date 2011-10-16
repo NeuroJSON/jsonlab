ad1102b186bd39e69b0e02b114f668f42a83dd51
fangq <fangq@786e58fb-9377-0410-9ff7-e4ac0ac0635c>
2011-10-16 03:52:34 +0000

    add json demo and regression testing units, support cpx numbers, fix double quote bug
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/jsonlab@311 786e58fb-9377-0410-9ff7-e4ac0ac0635c

---
+++ examples/jsonlab_selftest.m
@@ -0,0 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%         Regression Test Unit of loadjson and savejson
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+for i=1:3
+    fname=sprintf('example%d.json',i);
+    if(exist(fname,'file')==0) break; end
+    fprintf(1,'===============================================\n>> %s\n',fname);
+    json=savejson('data',loadjson(fname));
+    fprintf(1,'%s\n',json);
+    data=loadjson(json);
+end
