1736eb403b397f1940094e86bd5367e8f06774a9
fangq <fangq@786e58fb-9377-0410-9ff7-e4ac0ac0635c>
2011-11-19 04:01:14 +0000

    add test sample for struct array
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/jsonlab@327 786e58fb-9377-0410-9ff7-e4ac0ac0635c

--- examples/jsonlab_selftest.m
+++ examples/jsonlab_selftest.m
@@ -2,7 +2,7 @@
 %         Regression Test Unit of loadjson and savejson
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-for i=1:3
+for i=1:4
     fname=sprintf('example%d.json',i);
     if(exist(fname,'file')==0) break; end
     fprintf(1,'===============================================\n>> %s\n',fname);
