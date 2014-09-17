ec151dcfab9f5e1ca5ed2033977974d9b0fe75f3
Qianqian Fang <fangq@nmr.mgh.harvard.edu>
2014-09-17 05:31:08 +0000

    fixing everything for GNU octave
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/jsonlab@439 786e58fb-9377-0410-9ff7-e4ac0ac0635c

--- savejson.m
+++ savejson.m
@@ -414,8 +414,20 @@ end
 
 %%-------------------------------------------------------------------------
 function newstr=escapejsonstring(str)
-escapechars={'\a','\b','\f','\n','\r','\t','\v'};
 newstr=str;
-for i=1:length(escapechars);
-  newstr=regexprep(newstr,escapechars{i},regexprep(escapechars{i},'\\','\\\\'));
+isoct=exist('OCTAVE_VERSION','builtin');
+if(isoct)
+   vv=sscanf(OCTAVE_VERSION,'%f');
+   if(vv(1)>=3.8) isoct=0; end
+end
+if(isoct)
+  escapechars={'\a','\f','\n','\r','\t','\v'};
+  for i=1:length(escapechars);
+    newstr=regexprep(newstr,escapechars{i},escapechars{i});
+  end
+else
+  escapechars={'\a','\b','\f','\n','\r','\t','\v'};
+  for i=1:length(escapechars);
+    newstr=regexprep(newstr,escapechars{i},regexprep(escapechars{i},'\\','\\\\'));
+  end
 end
