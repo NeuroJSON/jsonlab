3f6bfedb16ff8f13dd53d3078452dbbbea6c383d
fangq <fangq@786e58fb-9377-0410-9ff7-e4ac0ac0635c>
2015-05-05 19:21:20 +0000

    making options case insensitive
    git-svn-id: http://svn.code.sf.net/p/iso2mesh/code/trunk/jsonlab@484 786e58fb-9377-0410-9ff7-e4ac0ac0635c

--- jsonopt.m
+++ jsonopt.m
@@ -26,7 +26,10 @@ function val=jsonopt(key,default,varargin)
 val=default;
 if(nargin<=2) return; end
 opt=varargin{1};
-if(isstruct(opt) && isfield(opt,key))
-    val=getfield(opt,key);
+if(isstruct(opt))
+    if(isfield(opt,key))
+       val=getfield(opt,key);
+    elseif(isfield(opt,lower(key)))
+       val=getfield(opt,lower(key));
+    end
 end
-
