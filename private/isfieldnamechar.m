413b85a1b6f40398888c95c329381a9ffd81def2
oleg komarov <o.komarov11@imperial.ac.uk>
2015-12-24 18:41:42 +0000

    environment independet check (also faster)

--- private/isfieldnamechar.m
+++ private/isfieldnamechar.m
@@ -3,10 +3,6 @@ function tf = isfieldnamechar(str)
 % See also: ISVARNAME
 
 % ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz
-validchars = [65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,95,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122];
-if isOctave()
-    tf = ismember(double(str), validchars);
-else
-    tf = ismembc(double(str), validchars);
-end
+% validchars = [65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,95,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122];
+tf = (str > 64 & str < 91) | str == 95 | (str > 96 & str < 123);
 end
